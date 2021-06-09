68296e0da40c2e4107cf9d103b12ce5ed90fe3ec
Osvaldo <osvarz.new@gmail.com>
2021-06-09 06:05:55 -0500

    💄 layout component created

--- components/header/index.js
+++ components/header/index.js
@@ -5,13 +5,15 @@ const Header = () => {
   return (
     <header
       id="header"
-      className="text-white bg-dark text-center lg:text-right flex md:block lg:flex justify-between items-center border border-grey-light py-4 md:py-8-md lg:py-5-lg xl:py-5-xl"
+      className="text-white bg-dark text-center lg:text-right border border-grey-light py-4 md:py-8-md lg:py-5-lg xl:py-5-xl"
     >
-      <div className="font-antonio mb-0 md:mb-8-md lg:mb-0 text-xl md:text-xl-md lg:text-xl-lg xl:text-xl-xl leading-3xl md:leading-3xl-md lg:leading-3xl-lg xl:leading-3xl-xl">
-        <h1>THE PLANETS</h1>
+      <div className="w-11/12 mx-auto flex md:block lg:flex justify-between items-center">
+        <div className="font-antonio mb-0 md:mb-8-md lg:mb-0 text-xl md:text-xl-md lg:text-xl-lg xl:text-xl-xl leading-3xl md:leading-3xl-md lg:leading-3xl-lg xl:leading-3xl-xl">
+          <h1>THE PLANETS</h1>
+        </div>
+        <Navbar />
+        <NavbarResponsive />
       </div>
-      <Navbar />
-      <NavbarResponsive />
     </header>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,9 @@
-import Header from 'components/header'
+import Layout from 'components/layout'
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <main className="min-h-screen flex justify-center items-center mb-5-xs md:mb-5-md lg:mb-5-lg xl:mb-5-xl">
-        <h1 className="font-spartan text-center text-4xl-xl md:text-4xl-md lg:text-4xl-lg xl:text-4xl-xl">
-          Welcome to{' '}
-          <a
-            href="https://nextjs.org"
-            className="font-antonio md:text-grey-dark text-orange-own"
-          >
-            Next.js!
-          </a>
-        </h1>
-      </main>
+      <Layout></Layout>
     </>
   )
 }
