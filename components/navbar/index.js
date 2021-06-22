19356a14e7430c188f8be223434b1de00feff856
Osvaldo <osvarz.new@gmail.com>
2021-06-22 07:59:35 -0500

    ⚡️ interaction added in header

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,18 +1,99 @@
+import { useEffect, useState } from 'react'
 import NavLink from './nav-link'
 
 const Navbar = () => {
+  const [active, setActive] = useState(null)
+
+  const changeActive = (element) => {
+    active.style = 'color: #c1c1c8'
+    element.style = `color: #${element.id}`
+  }
+
+  useEffect(() => {
+    const tab = document.getElementsByClassName('mercury')
+    setActive(tab[0])
+    if (!active) {
+      tab[0].style = 'color: #419EBB'
+    }
+  }, [])
+
   return (
     <div className="text-white-opacity hidden md:block font-spartan font-bold text-sm leading-lg">
       <nav>
         <ul className="flex items-center justify-center lg:justify-start">
-          <NavLink title="mercury" href="/mercury/overview" />
-          <NavLink title="venus" href="/venus/overview" />
-          <NavLink title="earth" href="/earth/overview" />
-          <NavLink title="mars" href="/mars/overview" />
-          <NavLink title="jupiter" href="/jupiter/overview" />
-          <NavLink title="saturn" href="/saturn/overview" />
-          <NavLink title="uranus" href="/uranus/overview" />
-          <NavLink title="neptune" href="/neptune/overview" />
+          <NavLink
+            title="mercury"
+            id="419EBB"
+            className="mercury"
+            href="/mercury/overview"
+            onClick={(e) => {
+              changeActive(e.target)
+              setActive(e.target)
+            }}
+          />
+          <NavLink
+            title="venus"
+            id="EDA249"
+            href="/venus/overview"
+            onClick={(e) => {
+              changeActive(e.target)
+              setActive(e.target)
+            }}
+          />
+          <NavLink
+            title="earth"
+            id="6D2ED5"
+            href="/earth/overview"
+            onClick={(e) => {
+              changeActive(e.target)
+              setActive(e.target)
+            }}
+          />
+          <NavLink
+            title="mars"
+            id="D14C32"
+            href="/mars/overview"
+            onClick={(e) => {
+              changeActive(e.target)
+              setActive(e.target)
+            }}
+          />
+          <NavLink
+            title="jupiter"
+            id="D83A34"
+            href="/jupiter/overview"
+            onClick={(e) => {
+              changeActive(e.target)
+              setActive(e.target)
+            }}
+          />
+          <NavLink
+            title="saturn"
+            id="CD5120"
+            href="/saturn/overview"
+            onClick={(e) => {
+              changeActive(e.target)
+              setActive(e.target)
+            }}
+          />
+          <NavLink
+            title="uranus"
+            id="1EC1A2"
+            href="/uranus/overview"
+            onClick={(e) => {
+              changeActive(e.target)
+              setActive(e.target)
+            }}
+          />
+          <NavLink
+            title="neptune"
+            id="2D68F0"
+            href="/neptune/overview"
+            onClick={(e) => {
+              changeActive(e.target)
+              setActive(e.target)
+            }}
+          />
         </ul>
       </nav>
     </div>
--- components/navbar/nav-link/index.js
+++ components/navbar/nav-link/index.js
@@ -1,10 +1,20 @@
 import Link from 'next/link'
 
-const NavLink = ({ title = 'title', href = '#' }) => {
+const NavLink = ({
+  title = 'title',
+  href = '#',
+  id,
+  className = '',
+  onClick = () => {},
+}) => {
   return (
     <li>
       <Link href={href}>
-        <a className="uppercase mx-3-xs md:mx-3-md lg:mx-3-lg xl:mx-3-xl">
+        <a
+          className={`${className} uppercase mx-3-xs md:mx-3-md lg:mx-3-lg xl:mx-3-xl`}
+          id={id}
+          onClick={onClick}
+        >
           {title}
         </a>
       </Link>
