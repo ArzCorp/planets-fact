160c538ba1b7cf0707e666fb418bc51a638fe33b
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-10-01 22:11:41 -0600

    🍱 add overview data on plantes

--- app/utils/constants/staticData.js
+++ app/utils/constants/staticData.js
@@ -6,6 +6,9 @@ export const PLANETS = [
     planet_color: '#DEF4FC',
     overview:
       "Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.",
+    structure: `Mercury appears to have a solid silicate crust and mantle overlying a solid, iron sulfide outer core layer, a deeper liquid core layer, and a solid inner core. The planet's density is the second highest in the Solar System at 5.427 g/cm3 , only slightly less than Earth's density.`,
+    surface: ``,
+    image_size: 290,
   },
   {
     id: 2,
@@ -13,6 +16,9 @@ export const PLANETS = [
     planet_color_class: 'bg-[#F7CC7F]',
     planet_color: '#F7CC7F',
     overview: `Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty. As the brightest natural object in Earth's night sky after the Moon, Venus can cast shadows and can be, on rare occasions, visible to the naked eye in broad daylight.`,
+    structure: ``,
+    surface: ``,
+    image_size: 290,
   },
   {
     id: 3,
@@ -20,6 +26,9 @@ export const PLANETS = [
     planet_color_class: 'bg-[#545BFE]',
     planet_color: '#545BFE',
     overview: `Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth's surface is land with remaining 70.8% is covered with water. Earth's distance from the Sun, physical properties and geological history have allowed life to evolve and thrive.`,
+    structure: ``,
+    surface: ``,
+    image_size: 290,
   },
   {
     id: 4,
@@ -28,6 +37,9 @@ export const PLANETS = [
     planet_color: '#FF6A45',
     overview:
       'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the "Red Planet".',
+    structure: ``,
+    surface: ``,
+    image_size: 290,
   },
   {
     id: 5,
@@ -36,6 +48,9 @@ export const PLANETS = [
     planet_color: '#ECAD7A',
     overview:
       'Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass two and a half times that of all the other planets in the Solar System combined, but less than one-thousandth the mass of the Sun.',
+    structure: ``,
+    surface: ``,
+    image_size: 290,
   },
   {
     id: 6,
@@ -44,6 +59,9 @@ export const PLANETS = [
     planet_color: '#FCCB6B',
     overview:
       'Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas giant with an average radius of about nine and a half times that of Earth. It only has one-eighth the average density of Earth.',
+    structure: ``,
+    surface: ``,
+    image_size: 290,
   },
   {
     id: 7,
@@ -52,6 +70,9 @@ export const PLANETS = [
     planet_color: '#65F0D5',
     overview:
       'Uranus is the seventh planet from the Sun. Its name is a reference to the Greek god of the sky, Uranus according to Greek mythology, was the great-grandfather of Ares. It has the third-largest planetary radius and fourth-largest planetary mass in the Solar System.',
+    structure: ``,
+    surface: ``,
+    image_size: 290,
   },
   {
     id: 8,
@@ -60,6 +81,9 @@ export const PLANETS = [
     planet_color: '#497EFA',
     overview:
       'Neptune is the eighth and farthest-known Solar planet from the Sun. In the Solar System, it is the fourth-largest planet by diameter, the third-most-massive planet, and the densest giant planet. It is 17 times the mass of Earth, more massive than its near-twin Uranus.',
+    structure: ``,
+    surface: ``,
+    image_size: 290,
   },
 ]
 
